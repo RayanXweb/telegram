9cb09eb91b4281e72cb3c04f2ec47035c1cb6499
RayanXweb <rohmanjakarta15@gmail.com>
2026-04-22 05:12:14 +0700

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
 
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+const rand = (a,b)=>Math.floor(Math.random()*(b-a+1))+a;
+
+function loadJSON(path, def={}) {
+    return fs.existsSync(path) ? JSON.parse(fs.readFileSync(path)) : def;
+}
+
+function saveJSON(path, data) {
+    fs.writeFileSync(path, JSON.stringify(data, null, 2));
+}
+
+function log(text) {
+    console.log(text);
+    fs.appendFileSync("data/logs.txt", text + "\n");
+}
+
+module.exports = { sleep, rand, loadJSON, saveJSON, log };
