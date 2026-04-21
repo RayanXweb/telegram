2b4d77557c1cf092b9d9c7eba71039676755859d
RayanXweb <rohmanjakarta15@gmail.com>
2026-04-22 05:13:01 +0700

    Update inviter.js

--- modules/inviter.js
+++ modules/inviter.js
@@ -1,1 +1,40 @@
+const { sleep, rand, saveJSON, log } = require("./utils");
 
+async function invite(client, channel, users, resume, config) {
+    let count = 0;
+
+    for (let user of users) {
+
+        if (resume[user.id]) continue;
+        if (!user.username) continue;
+
+        try {
+            log(`➕ Add ${user.username}`);
+
+            await client.invoke({
+                _: "channels.inviteToChannel",
+                channel,
+                users: [user.id]
+            });
+
+            resume[user.id] = true;
+            count++;
+
+            if (count >= config.maxAddPerDay) break;
+
+            await sleep(rand(config.delayMin, config.delayMax));
+
+        } catch (e) {
+            log(`❌ ${e.message}`);
+
+            if (e.message.includes("FLOOD_WAIT")) {
+                let w = parseInt(e.message.match(/\d+/)[0]);
+                await sleep(w * 1000);
+            } else break;
+        }
+    }
+
+    saveJSON("data/resume.json", resume);
+}
+
+module.exports = invite;
