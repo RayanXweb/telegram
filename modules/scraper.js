2beb9e6c29c2fbf0bf3f0ec2941cba8ff33f30bb
RayanXweb <rohmanjakarta15@gmail.com>
2026-04-22 05:12:37 +0700

    Update scraper.js

--- modules/scraper.js
+++ modules/scraper.js
@@ -1,1 +1,20 @@
+const { log, saveJSON } = require("./utils");
 
+async function scrape(client, group) {
+    log("🔍 Scraping member...");
+
+    const participants = await client.getParticipants(group);
+
+    const users = participants.map(u => ({
+        id: u.id,
+        username: u.username,
+        name: u.firstName
+    }));
+
+    saveJSON("data/users.json", users);
+
+    log(`✔️ Total user: ${users.length}`);
+    return users;
+}
+
+module.exports = scrape;
